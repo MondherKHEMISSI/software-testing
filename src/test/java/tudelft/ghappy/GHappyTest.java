bfa277ceeef95b39d8de414553b51b62c207c6d9
MondherKHEMISSI <monder850@gmail.com>
2023-11-16 07:23:00 +0100

    Initial commit

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,31 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+    private GHappy gh;
+
+    @BeforeEach
+    public void initialize() {this.gh = new GHappy();}
+
+    @Test
+    public void trueOccurence(){
+        boolean result = gh.gHappy("xxggxx");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void falseOccurence(){
+        boolean result = gh.gHappy("xxgxx");
+        Assertions.assertFalse(result);
+    }
+
+
+    @Test
+    public void flaseMultiOccurence(){
+        boolean result = gh.gHappy("xxggyygxx");
+        Assertions.assertFalse(result);
+    }
 }
